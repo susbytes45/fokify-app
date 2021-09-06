71ddc27dcab3583d1063c7d56138e24ddfafbf8b
sushil4034 <sushilmaurya1174034@gmail.com>
2021-09-06 10:44:16 +0530

    clean up builder

--- src/js/controller.js
+++ src/js/controller.js
@@ -106,7 +106,7 @@ const controlAddRecipe = async function (newRecipe) {
     // Render bookmark view
     bookmarksView.render(model.state.bookmarks);
 
-    // Change ID in URL
+    // Change ID in URL link
     window.history.pushState(null, '', `#${model.state.recipe.id}`);
 
     // Close form window
