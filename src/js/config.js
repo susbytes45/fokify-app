efc82b9269e5a25a06d04ba1e9c8a015efab494e
sushil4034 <sushilmaurya1174034@gmail.com>
2021-09-06 09:20:03 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '34df3347-f6e8-4bd7-8c78-783b39457f28';
+export const MODAL_CLOSE_SEC = 2.5;
