98c9b2317855dcfb59f461893727bfc217d1d8a1
sushil4034 <sushilmaurya1174034@gmail.com>
2021-09-06 14:09:12 +0530

    fraction edit

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -129,7 +129,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new numberToFraction(ing.quantity).toString() : ''
+        ing.quantity ? numberToFraction(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
